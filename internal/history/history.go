f960742b763e8047ae8f910a9192771ddb32c3b6
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-08-07 09:18:09 -0300

    feat: adding history

--- internal/history/history.go
+++ internal/history/history.go
@@ -4,6 +4,27 @@ package history
 var commands []string
 var index int = -1
 
-func Add()  {
-	
+func Add(command string)  {
+	if command != ""{
+		commands = append(commands, command)
+		index = len(command) // sempre depois do ultimo
+	}
+}
+
+func Prev() string  {
+	if len(commands) == 0 || index <= 0{
+		return ""
+	}
+	index--
+	return commands[index]
+}
+
+func Next() string{
+	if len(commands) == 0 || index >= len(commands)-1{
+		index = len(commands)
+		return ""
+	}
+
+	index++
+	return commands[index]
 }
